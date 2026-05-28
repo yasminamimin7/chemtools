d78ede3f0dbd0af6b04a02b78d5d4fb04cff124d
yasminamimin7 <yasminasyarifah393@gmail.com>
2026-05-28 21:03:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -872,7 +872,7 @@ if menu == "📊 Dashboard":
             "Tips Belajar": 10
         }
         
-        
+               fig = go.Figure(data=[ go.Bar(
             
                 x=list(aktivitas.keys()),
                 y=list(aktivitas.values()),
