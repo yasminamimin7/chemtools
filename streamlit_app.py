a043bde696595f86aa91407870ed3594cfb16530
yasminamimin7 <yasminasyarifah393@gmail.com>
2026-06-08 05:13:42 +0700

    Update rating and feedback components in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -355,16 +355,23 @@ if menu == "🏠 Beranda":
     </div>
     """, unsafe_allow_html=True)
     
-st.markdown("### ⭐ Beri Rating WEBSITE ini")
-
-    rating = st.radio(
-        "Seberapa puas kamu dengan SPEKTRA?",
-       import streamlit as st
+st.header("Bagaimana pengalamanmu menggunakan website ini?")
+import streamlit as st
 
 sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+import streamlit as st
+import streamlit as st
+
+sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+selected = st.feedback("thumbs")
+if selected is not None:
+    st.markdown(f"You selected: {sentiment_mapping[selected]}")
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
 
 
     st.markdown("## 💬 Kritik dan Saran")
