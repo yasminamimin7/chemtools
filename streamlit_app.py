37c77cde35b14898a73c303b37affed4bbb58db6
yasminamimin7 <yasminasyarifah393@gmail.com>
2026-06-09 04:33:50 +0700

    Refactor import and feedback sections in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -355,19 +355,18 @@ if menu == "🏠 Beranda":
     </div>
     """, unsafe_allow_html=True)
     
-st.header("Bagaimana pengalamanmu menggunakan website ini?")
-import streamlit as st
+    st.header("Bagaimana pengalamanmu menggunakan website ini?")
+    import streamlit as st
 
-sentiment_mapping = ["one", "two", "three", "four", "five"]
-selected = st.feedback("stars")
-if selected is not None:
-    st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
-import streamlit as st
-import streamlit as st
+    sentiment_mapping = ["one", "two", "three", "four", "five"]
+    selected = st.feedback("stars")
+    if selected is not None:
+        st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
 
-sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
-selected = st.feedback("thumbs")
-if selected is not None:
+
+    sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+    selected = st.feedback("thumbs")
+    if selected is not None:
 
     st.markdown("## 💬 Kritik dan Saran")
 
